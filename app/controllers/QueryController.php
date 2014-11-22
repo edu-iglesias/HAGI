66c287ac44a9807c42251851657f76c1b360cb10
johnarben2468 <johnarbennicolehombrebueno@yahoo.com.ph>
2014-11-23 00:55:00 +0800

    Adds tablelist function

--- app/controllers/QueryController.php
+++ app/controllers/QueryController.php
@@ -6,4 +6,52 @@ class QueryController extends BaseController {
 	{
 		return View::make('query');
 	}
+	public function columnList()
+	{
+
+
+	if($tablename=="Awarding")
+		$tableCode = get_award();
+	else if($tablename=="Bidders")
+		$tableCode = get_bidders();
+	else if($tablename=="Organization")
+		$tableCode = get_organization();
+	else if($tablename=="Bid Line Item")
+		$tableCode = get_bid_line_item();
+	else if($tablename=="Bid Information")
+		$tableCode = get_bid_information();
+	else if($tablename=="Project Location")
+		$tableCode = get_project_location();
+	else if($tablename=="Organization Business Category")
+	
+
+
+	$rows[$tablename] = gen_query_getrow($tablename);
+
+	// transform object to array
+		$mainstring = "";
+		foreach ($rows[$tablename] as $key) 
+		{
+			$cols = (Array) $key;
+			foreach ($cols as $col) 
+			{
+			$mainstring[] .= ', '.$col;
+			}
+		}
+
+	
+}
+public function tableList()
+{
+	return array(
+	"Awarding",
+	"Bidders",
+	"Organization",
+	"Bid Line Item",
+	"Bid Information",
+	"Project Location",
+	"Organization Business Category"
+		);
+}
+
 }
