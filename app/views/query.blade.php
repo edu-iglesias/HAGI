3e7ab236098276f9f75e84c4d5eb2bcf0f85f556
Edu Iglesias <duthesecond@gmail.com>
2014-11-23 02:04:12 +0800

    add dynamic fieldname

--- app/views/query.blade.php
+++ app/views/query.blade.php
@@ -46,44 +46,105 @@
             @for ($i = 0; $i < 3; $i++)
             <tr>
                 <td style="width:100px;">
-                    <p><input class="form-control" list="tableName" onchange="changeTable(this.value)">
+                    <p><input class="form-control" list="tableName" onchange="changeTable(this.value)"  @if($i != 0) disabled @endif>
                         <datalist id="tableName">
-                            <option value="Award"><option value="Bid">
+                            <option value="Awarding">
+                            <option value="Bidders">
+                            <option value= "Bid Line Item">
+                            <option value="Bid Information">
+                            <option value="Award">
+                            <option value="Project Location">
+                            <option value="Organization Business Category">
                         </datalist>
                     </p>
                 </td>
-<!-- 
-                <td style="width:100px;">
-                    <p><input class="form-control" list="fieldName">
+
+                <td id="tb0" style="width:100px;">
+                    <p><input class="form-control" list="fieldName" disabled>
                         <datalist id="fieldName">
                             <option value="AwardNo">
                             <option value="Ko">
                         </datalist></p>
-                </td> -->
+                </td>
 
-                <td style="width:100px;">
-                    <p><input class="form-control" list="fieldName">
-                        <datalist id="fieldName">
-                            @foreach(tb1 as $key)
+                <td id="tb1"  style="display:none;">
+                    <p><input class="form-control" list="fieldName1">
+                        <datalist id="fieldName1">
+                            @foreach($tb1 as $key)
                                 <option value="{{ $key }}">
                             @endforeach
                         </datalist></p>
                 </td>
 
-                <td style="width:100px;">
+                <td id="tb2" style="display:none;">
+                    <p><input class="form-control" list="fieldName2">
+                        <datalist id="fieldName2">
+                            @foreach($tb2 as $key)
+                                <option value="{{ $key }}">
+                            @endforeach
+                        </datalist></p>
+                </td>
+
+                <td id="tb3" style="display:none;">
+                    <p><input class="form-control" list="fieldName3">
+                        <datalist id="fieldName3">
+                            @foreach($tb3 as $key)
+                                <option value="{{ $key }}">
+                            @endforeach
+                        </datalist></p>
+                </td>
+
+                <td id="tb4" style="display:none;">
+                    <p><input class="form-control" list="fieldName4">
+                        <datalist id="fieldName4">
+                            @foreach($tb4 as $key)
+                                <option value="{{ $key }}">
+                            @endforeach
+                        </datalist></p>
+                </td>
+
+                <td id="tb5" style="display:none;">
+                    <p><input class="form-control" list="fieldName5">
+                        <datalist id="fieldName5">
+                            @foreach($tb5 as $key)
+                                <option value="{{ $key }}">
+                            @endforeach
+                        </datalist></p>
+                </td>
+
+                <td id="tb6" style="display:none;">
+                    <p><input class="form-control" list="fieldName6">
+                        <datalist id="fieldName6">
+                            @foreach($tb6 as $key)
+                                <option value="{{ $key }}">
+                            @endforeach
+                        </datalist></p>
+                </td>
+
+                <td id="tb7" style="display:none;">
+                    <p><input class="form-control" list="fieldName7">
+                        <datalist id="fieldName7">
+                            @foreach($tb7 as $key)
+                                <option value="{{ $key }}">
+                            @endforeach
+                        </datalist></p>
+                </td>
+
+
+                <td >
                     <p><input type="radio" name="sort" value="ascending" checked> Ascending &nbsp;<br> 
                         <input type="radio" name="sort" value="descending" > Descending
                 </td>
 
-                <td style="width:100px;">
+                <td >
                     <p><input type="checkbox" name="show">
                 </td>
 
-                <td style="width:100px;">
+                <td >
                     <p><input type="checkbox" name="unique"> 
                 </td>
 
-                <td style="width:100px;">
+                <td>
                     <p><input class="form-control" list="operator">
                         <datalist id="operator">
                             <option value=">">
@@ -138,7 +199,7 @@
             var cell7 = row.insertCell(6);
             var cell8 = row.insertCell(7);
 
-            cell1.innerHTML = "<td style=\"width:100px;\"><p><input class=\"form-control\" list=\"tableName\"><datalist><option value=\"Award\"><option value=\"Bid\"></datalist></p></td>";
+            cell1.innerHTML = "<td style=\"width:100px;\"><p><input disabled class=\"form-control\" list=\"tableName\"><datalist><option value=\"Award\"><option value=\"Bid\"></datalist></p></td>";
             cell2.innerHTML = "<td id=\"fieldName\" style=\"width:100px;\"><p><input class=\"form-control\" list=\"fieldName\"><datalist><option value=\"AwardNo\"><option value=\"Ko\"></datalist></p></td>";
             cell3.innerHTML = "<td style=\"width:100px;\"><p><input type=\"radio\" name=\"sort\" value=\"ascending\" checked> Ascending &nbsp;<br> <input type=\"radio\" name=\"sort\" value=\"descending\" > Descending";
             cell4.innerHTML = "<td style=\"width:100px;\"><p><input type=\"checkbox\" name=\"show\">";
@@ -153,54 +214,96 @@
             document.getElementById("myForm").reset();
         }
 
-        function changeTable(value, tablename)
+        function changeTable(value)
         {
             window.base = 'SELECT * FROM';
             window.tableName = value;
 
-            switch(tablename) 
+            switch(value) 
             {
-                case "haha":
-                    alert('haha');
-                    document.getElementById('fieldName').style.display = "";
-                    document.getElementById('fieldName').style.display = "";
-                    document.getElementById('fieldName').style.display = "";
-                    document.getElementById('fieldName').style.display = "";
-                    document.getElementById('fieldName').style.display = "";
-                    document.getElementById('fieldName').style.display = "";
-                    document.getElementById('fieldName').style.display = "";
+                case "Awarding":
+                    document.getElementById('tb0').style.display = "none";
+                    document.getElementById('tb1').style.display = "table-cell";
+                    document.getElementById('tb2').style.display = "none";
+                    document.getElementById('tb3').style.display = "none";
+                    document.getElementById('tb4').style.display = "none";
+                    document.getElementById('tb5').style.display = "none";
+                    document.getElementById('tb6').style.display = "none";
+                    document.getElementById('tb7').style.display = "none";
                     break;
-                case "hehe":
-                alert('haha');
-                    document.getElementById('fieldName').style.display = "";
-                    document.getElementById('fieldName').style.display = "";
-                    document.getElementById('fieldName').style.display = "";
-                    document.getElementById('fieldName').style.display = "";
-                    document.getElementById('fieldName').style.display = "";
-                    document.getElementById('fieldName').style.display = "";
-                    document.getElementById('fieldName').style.display = "";
+                case "Bidders":
+                    document.getElementById('tb0').style.display = "none";
+                    document.getElementById('tb1').style.display = "none";
+                    document.getElementById('tb2').style.display = "table-cell";
+                    document.getElementById('tb3').style.display = "none";
+                    document.getElementById('tb4').style.display = "none";
+                    document.getElementById('tb5').style.display = "none";
+                    document.getElementById('tb6').style.display = "none";
+                    document.getElementById('tb7').style.display = "none";
                     break;
-                case n:
-                    document.getElementById('fieldName').style.display = "";
-                    document.getElementById('fieldName').style.display = "";
-                    document.getElementById('fieldName').style.display = "";
-                    document.getElementById('fieldName').style.display = "";
-                    document.getElementById('fieldName').style.display = "";
-                    document.getElementById('fieldName').style.display = "";
-                    document.getElementById('fieldName').style.display = "";
+                case "Organization":
+                    document.getElementById('tb0').style.display = "none";
+                    document.getElementById('tb1').style.display = "none";
+                    document.getElementById('tb2').style.display = "none";
+                    document.getElementById('tb3').style.display = "table-cell";
+                    document.getElementById('tb4').style.display = "none";
+                    document.getElementById('tb5').style.display = "none";
+                    document.getElementById('tb6').style.display = "none";
+                    document.getElementById('tb7').style.display = "none";
                     break;
-                case n:
-                    document.getElementById('fieldName').style.display = "";
-                    document.getElementById('fieldName').style.display = "";
-                    document.getElementById('fieldName').style.display = "";
-                    document.getElementById('fieldName').style.display = "";
-                    document.getElementById('fieldName').style.display = "";
-                    document.getElementById('fieldName').style.display = "";
-                    document.getElementById('fieldName').style.display = "";
+                case "Bid Line Item":
+                    document.getElementById('tb0').style.display = "none";
+                    document.getElementById('tb1').style.display = "none";
+                    document.getElementById('tb2').style.display = "none";
+                    document.getElementById('tb3').style.display = "none";
+                    document.getElementById('tb4').style.display = "table-cell";
+                    document.getElementById('tb5').style.display = "none";
+                    document.getElementById('tb6').style.display = "none";
+                    document.getElementById('tb7').style.display = "none";
+                    break;
+                case "Bid Information":
+                    document.getElementById('tb0').style.display = "none";
+                    document.getElementById('tb1').style.display = "none";
+                    document.getElementById('tb2').style.display = "none";
+                    document.getElementById('tb3').style.display = "none";
+                    document.getElementById('tb4').style.display = "none";
+                    document.getElementById('tb5').style.display = "table-cell";
+                    document.getElementById('tb6').style.display = "none";
+                    document.getElementById('tb7').style.display = "none";
+                    break;
+                case "Project Location":
+                    document.getElementById('tb0').style.display = "none";
+                    document.getElementById('tb1').style.display = "none";
+                    document.getElementById('tb2').style.display = "none";
+                    document.getElementById('tb3').style.display = "none";
+                    document.getElementById('tb4').style.display = "none";
+                    document.getElementById('tb5').style.display = "none";
+                    document.getElementById('tb6').style.display = "table-cell";
+                    document.getElementById('tb7').style.display = "none";
+                    break;
+                case "Organization Business Category":
+                    document.getElementById('tb0').style.display = "none";
+                    document.getElementById('tb1').style.display = "none";
+                    document.getElementById('tb2').style.display = "none";
+                    document.getElementById('tb3').style.display = "none";
+                    document.getElementById('tb4').style.display = "none";
+                    document.getElementById('tb5').style.display = "none";
+                    document.getElementById('tb6').style.display = "none";
+                    document.getElementById('tb7').style.display = "table-cell";
                     break;
 
                 default:
-                    default code block
+                    document.getElementById('tb0').style.display = "table-cell";
+                    document.getElementById('tb1').style.display = "none";
+                    document.getElementById('tb2').style.display = "none";
+                    document.getElementById('tb3').style.display = "none";
+                    document.getElementById('tb4').style.display = "none";
+                    document.getElementById('tb5').style.display = "none";
+                    document.getElementById('tb6').style.display = "none";
+                    document.getElementById('tb7').style.display = "none";
+                    break;
+
+
             }
 
 
