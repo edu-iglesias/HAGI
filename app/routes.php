b0abb03074792f39d136b17964627ac559259fa5
johnarben2468 <johnarbennicolehombrebueno@yahoo.com.ph>
2014-11-22 17:09:00 +0800

    Adds query to api functions

--- app/routes.php
+++ app/routes.php
@@ -17,20 +17,14 @@ Route::get('/', function()
 });
 
 
-Route::get('/register', function()
+//
+Route::get('/test', function()
 {
-	return View::make('register');
+	$sql = 'SELECT * FROM "'.get_award().'" LIMIT 5';
+	
+	$results = get_query($sql);
+
+	dd($results);
 });
 //
 
-// Confide routes
-Route::get('users/create', 'UsersController@create');
-Route::post('users', 'UsersController@store');
-Route::get('users/login', 'UsersController@login');
-Route::post('users/login', 'UsersController@doLogin');
-Route::get('users/confirm/{code}', 'UsersController@confirm');
-Route::get('users/forgot_password', 'UsersController@forgotPassword');
-Route::post('users/forgot_password', 'UsersController@doForgotPassword');
-Route::get('users/reset_password/{token}', 'UsersController@resetPassword');
-Route::post('users/reset_password', 'UsersController@doResetPassword');
-Route::get('users/logout', 'UsersController@logout');
